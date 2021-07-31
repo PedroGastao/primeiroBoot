d6ac4571ae4016e67f5dc95615bac39b9e158998
Pedro Gastao <pedro.gastao.14@gmail.com>
2021-07-31 20:23:15 -0300

    configurando porta, express, adicionando script do start

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-console.log('Oi')
-console.log('segunda linha')
-console.log('terceira linha')
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.listen(PORT,() =>{
+    console.log(`App running on port ${PORT} `);
+})
 
