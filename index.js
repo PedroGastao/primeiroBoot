704472f2abd73d0678dd8396ed4b453eb0c5991f
Pedro Gastao <pedro.gastao.14@gmail.com>
2021-08-04 21:02:25 -0300

    implementando o cors

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+//importações
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 
+//invocação
 const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//subindo servidor
 app.listen(PORT,() =>{
     console.log(`App running on port ${PORT} `);
 })
